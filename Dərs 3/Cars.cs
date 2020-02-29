49a90a774fdfea78e3d094c06227ecf8f0dc0084
Elgun0 <57399241+Elgun0@users.noreply.github.com>
2020-02-29 20:42:56 +0400

    Add files via upload

---
+++ Dərs 3/Cars.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dərs_2
+{
+    class Cars
+    {
+        public string Marka;
+        public string Model;
+        public int FullPower;
+        public double FullKm;
+        public double CurrentFuel;
+        public double LocalKm;
+        public double GlobalKm;
+       
+        public Cars(string Marka, string Model, int FullPower, double FullKm, double CurrentFuel=0)
+        {
+            this.Marka = Marka;
+            this.Model = Model;
+            this.FullKm = FullKm;
+            this.FullPower = FullPower;
+            this.CurrentFuel = CurrentFuel;
+        }
+
+        public void Go()
+        {
+           
+            double neededKm = 0;
+
+            while (neededKm == 0)
+            {
+                Console.WriteLine("Nece km yol getmek isteyirsiniz?");
+                string userInput = Console.ReadLine();
+                if (IsEmpty(userInput))
+                {
+                    neededKm = Convert.ToDouble(userInput);
+                    if (CurrentFuel >= (neededKm / 100) * FullKm)
+                    {
+                        CurrentFuel -= (neededKm / 100) * FullKm;
+                        Console.WriteLine($"Siz {neededKm} qeder yol getdiniz. {CurrentFuel} litr benzininiz qaldi.");
+                        GlobalKm += neededKm;
+                        LocalKm += neededKm;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sizin masinda kifayet qeder benzin yoxdu :P . Zehmet olmasa benzi doldurun >:(");
+                    }
+                }
+            }
+
+        }
+        
+        public void TopUp()
+        {
+
+            Console.WriteLine("Nece litr benzin doldurmaq isteyirsiniz?");
+            double neededpower = 0;
+            string inp = Console.ReadLine();
+
+            while (neededpower == 0)
+            {
+                if (IsEmpty(inp))
+                {
+                neededpower = Convert.ToDouble(inp);
+                    if(CurrentFuel+neededpower<=FullPower)
+                    {
+                        CurrentFuel += neededpower;
+                        Console.WriteLine($"Benzin ugurla dolduruldu sizin {CurrentFuel}  benzininiz var");
+                    }else
+                    {
+                        Console.WriteLine("Sizin max benzininiz var");
+                    }
+
+                   
+                  
+                }
+            }
+
+        }
+
+        public void Stop()
+        {
+            LocalKm = 0;
+            Console.WriteLine($"Local km sifirlandi km: {GlobalKm}");
+        }
+
+        public void Lckm()
+        {
+            Console.WriteLine($"Global km: {GlobalKm}. Local km: {LocalKm}");
+        }
+        bool IsEmpty(string inp)
+        {
+            try
+            {
+                Convert.ToDouble(inp);
+                return true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please write number in the menu");
+                return false;
+            }
+        }
+    }
+}
---
+++ Dərs 3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dərs_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars car = new Cars("BMW", "M5", 80, 15, 50);
+            Cars mer = new Cars("Mercedes", "E350", 80, 15);
+            string input;
+            do
+            {
+                Console.WriteLine(">>>>>>>>>> Please write only button number <<<<<<<<<<");
+
+                Console.WriteLine("1. Go");
+                Console.WriteLine("2. Top up");
+                Console.WriteLine("3. Stop");
+                Console.WriteLine("4. Local km and Global km");
+                Console.WriteLine("5. Exit");
+
+                Console.WriteLine(">>>>>>>>>> AzPetrol | Azpetrol yaxsi yol :) <<<<<<<<<<");
+
+                input = Console.ReadLine();
+
+
+               
+
+                if (IsEmpty(input))
+                {
+                    switch (input)
+                    {
+                        case "1":
+                            Console.Clear();
+                            car.Go();
+
+                            break;
+                        case "2":
+                            Console.Clear();
+                            car.TopUp();
+
+                            break;
+                        case "3":
+                            Console.Clear();
+                            car.Stop();
+                            Console.WriteLine("Dayandiniz");
+                            break;
+                        case "4":
+                            Console.Clear();
+                            car.Lckm();  
+                            Console.WriteLine("Total");
+                            break;
+                        case "5":
+                            Console.Clear();
+                            Console.WriteLine("Tesekkurler");
+                            break;
+
+
+
+                        default:
+                            break;
+                    }
+                }
+
+            } while (input!="5");
+        }
+        
+        static bool IsEmpty(string inp)
+        {
+            try
+            {
+                Convert.ToInt32(inp);
+                return true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please write number in the menu");
+                return false;
+            }
+        }
+    }
+
+}
